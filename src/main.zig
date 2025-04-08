21d4401ae9578eed7e039d5d2c61bf9741d61c6a
KarolosLykos <Karolos.Lykos@powerfactors.com>
2025-04-08 16:51:30 +0300

    second stage

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,35 @@
 const std = @import("std");
 
+const print = std.debug.print;
+const MyErrors = error{TokenNotFound};
+const TokenType = enum {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    EOF,
+};
+
+const Token = struct {
+    tokenType: TokenType,
+    lexeme: []const u8,
+    literal: ?[]u8,
+};
+
+const EOFToken = Token{
+    .tokenType = .EOF,
+    .lexeme = "",
+    .literal = null,
+};
+const LPARENToken = Token{
+    .tokenType = .LEFT_PAREN,
+    .lexeme = "(",
+    .literal = null,
+};
+const RPARENToken = Token{
+    .tokenType = .RIGHT_PAREN,
+    .lexeme = ")",
+    .literal = null,
+};
+
 pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std.debug.print("Logs from your program will appear here!\n", .{});
@@ -25,8 +55,35 @@ pub fn main() !void {
 
     // Uncomment this block to pass the first stage
     if (file_contents.len > 0) {
-        @panic("Scanner not implemented");
+        for (file_contents) |i| {
+            try scanner(i);
+        }
     } else {
         try std.io.getStdOut().writer().print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
+    }
+}
+
+fn scanner(token: u8) !void {
+    if (token == '\n') {
+        return;
+    }
+    const t = try match(token);
+    try std.io.getStdOut().writer().print("{s} {s} {any}\n", .{ @tagName(t.tokenType), t.lexeme, t.literal });
+}
+
+fn match(token: u8) MyErrors!Token {
+    switch (token) {
+        '(' => {
+            return LPARENToken;
+        },
+        ')' => {
+            return RPARENToken;
+        },
+        0 => {
+            return EOFToken;
+        },
+        else => {
+            return MyErrors.TokenNotFound;
+        },
     }
 }
